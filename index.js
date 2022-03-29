075b01bafa36e9cffb1de40d1bdef461ddaad88e
David Kye <davidkye2.0@gmail.com>
2022-03-29 18:52:05 -0400

    commit tree 061f7b43b54b1da410afd6d7ceca8b83e02c2d9d

---
+++ index.js
@@ -0,0 +1,15 @@
+function spy(){
+
+}
+
+function receivesAFunction (spy){
+    spy();
+}
+
+function returnsANamedFunction(){
+    let fn = function(){}
+    return fn;
+}
+function returnsAnAnonymousFunction(){
+    return function(){}
+}
